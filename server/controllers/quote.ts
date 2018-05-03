98a4af54dbb3579d30dc71bf7ecaff6e34c673f2
ALESHA <mayur.mathurkar@hotmail.com>
2018-05-03 10:22:16 +0530

    pdf done

--- server/controllers/quote.ts
+++ server/controllers/quote.ts
@@ -2,6 +2,7 @@ import Quote from '../models/quote';
 import BaseCtrl from './base';
 // var JSZip = require('jszip');
 var HtmlDocx = require('html-docx-js');
+var pdf = require('html-pdf');
 var fs = require('fs');
 var path = require('path');
 
@@ -12,10 +13,15 @@ export default class QuoteCtrl extends BaseCtrl {
     let docpath = __dirname.replace('\dist', '').replace('server', 'client').replace('controllers', 'assets');
     console.log(path.resolve(docpath, 'html.docx'));
     var converted = HtmlDocx.asBlob(req.body.html);
-    let filename = req.body.name + '.docx';
-    fs.writeFile(path.resolve(docpath, filename), converted, function (err) {
+    let filename = req.body.name;
+    let pdfoptions = { format: 'Letter' };
+    fs.writeFile(path.resolve(docpath, filename + '.docx'), converted, function (err) {
       if (err) throw err;
+      pdf.create(req.body.html, pdfoptions).toFile(path.resolve(docpath, filename + '.pdf'), function (err, res) {
+        if (err) return console.log(err);
+        console.log(res); // { filename: '/app/businesscard.pdf' }
+      });
     });
-    res.status(200).json(path.resolve(docpath, filename));
+
   }
 }
